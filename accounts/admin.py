b9652ecba8e67e4720caa9c51e8afd2c8603f00b
frederik1824 <frederiklopez2418@gmail.com>
2024-06-28 13:30:49 -0400

    Este es mi primer pagquete commit de mi proyecto

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Account
+from .models import Account, UserProfile
+from django.utils.html import format_html
 
 class AccountAmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joind', 'is_active' )
@@ -11,5 +12,13 @@ class AccountAmin(UserAdmin):
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
+
+class UserprofileAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object.profile_picture.url))
+
+    thumbnail.short_description = 'Imagen de Perfil'
+    list_display = ['thumbnail', 'user' , 'city', 'state', 'country']
 # Register your models here.
 admin.site.register(Account, AccountAmin)
+admin.site.register(UserProfile, UserprofileAdmin)
